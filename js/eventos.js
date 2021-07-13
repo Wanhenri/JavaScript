3f5f360cf1b860f6fc7cd03e7bbcf4457ee205b9
Jessica Aquiles <jessica.aquiles@hotmail.com>
2021-07-13 00:26:31 -0300

    basico

--- js/eventos.js
+++ js/eventos.js
@@ -1,12 +1,14 @@
 //window é a janela document é o site a pagina web
-
+var contador = document.querySelector('.badge');
 
 document.addEventListener('click', event => {
+    var numero = parseInt(contador.textContent) + 1;
+    contador.textContent = numero;
     console.log('click');
 });
 
 
 window.addEventListener('focus', event => {
-    
+
     console.log('focus');
 });

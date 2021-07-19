d57c39c4699875dcfa5b6838bb25880b159dbb79
Jessica Aquiles <jessica.aquiles@hotmail.com>
2021-07-19 16:41:19 -0300

    basico

---
+++ js/number.js
@@ -0,0 +1,26 @@
+let num = 10;
+let numB = 2.5;
+
+
+
+//vai concatenar pq é string o num1
+console.log(num.toString() + numB);
+
+console.log(num);// volta a ser 10
+
+
+//para conferir o tiipo
+
+console.log(typeof num);
+
+//para deixar permanente em string
+num = num.toString();
+
+console.log(num.toString());
+
+//para conferir o tiipo
+
+console.log(typeof num);
+
+
+

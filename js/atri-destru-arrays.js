804b334e709597ee6f5177b179ee40de57e08422
Jessica Aquiles <jessica.aquiles@hotmail.com>
2021-07-24 20:15:18 -0300

    arrays

---
+++ js/atri-destru-arrays.js
@@ -0,0 +1,14 @@
+//                 0         1        2
+const numero = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+//               0  1  2    0  1  2    0  1  2
+
+const [um, dois] = numero;
+
+console.log(dois[0]);
+
+
+//const [um, , tres, ...resto] = numero;
+//console.log(um, tres);
+//console.log(resto);
+
+//... rest, ...spread
